80a8bb8961bd9859250325215bdef48b27004462
Jessie <jessie.zhao@yale.edu>
2016-12-02 01:08:32 +0000

    working on the design part

--- controllers/index.js
+++ controllers/index.js
@@ -9,8 +9,8 @@ var events = require('../models/events');
 function index (request, response) {
   var now = new Date();
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).',
+    'title': 'BestEvent',
+    'tagline': 'Discover Best Event in your life.',
     'events': events.all
   };
   for(var i=0; i < events.all.length; i++){
